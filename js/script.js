1854ddcc2a6314c90f576e4e72aa58ae099978f3
Vladimir <snegovikkvolodya@yandex.ru>
2024-12-18 16:02:42 +0300

    push all files

--- js/script.js
+++ js/script.js
@@ -132,3 +132,13 @@ function restoreFormData() {
     document.getElementById('message').value = data.message;
   }
 }
+
+
+// --FAQ
+const faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(item => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('active');
+  });
+});
